4555ca62ceb3b56e58d3876543b16c3deb1afd39
serica <943914044@qq.com>
2021-09-12 06:17:10 -0700

    wrapping integers passed

--- libsponge/wrapping_integers.cc
+++ libsponge/wrapping_integers.cc
@@ -1,12 +1,10 @@
 #include "wrapping_integers.hh"
-
+#include <limits>
+using namespace std;
 // Dummy implementation of a 32-bit wrapping integer
 
 // For Lab 2, please replace with a real implementation that passes the
 // automated checks run by `make check_lab2`.
-
-template <typename... Targs>
-void DUMMY_CODE(Targs &&... /* unused */) {}
 
 using namespace std;
 
@@ -14,8 +12,10 @@ using namespace std;
 //! \param n The input absolute 64-bit sequence number
 //! \param isn The initial sequence number
 WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
-    DUMMY_CODE(n, isn);
-    return WrappingInt32{0};
+    uint64_t relative_seqno = n + isn.raw_value();
+    auto seqno = static_cast<uint32_t>((relative_seqno) % (1ul << 32));
+
+    return WrappingInt32{seqno};
 }
 
 //! Transform a WrappingInt32 into an "absolute" 64-bit sequence number (zero-indexed)
@@ -29,6 +29,18 @@ WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
 //! and the other stream runs from the remote TCPSender to the local TCPReceiver and
 //! has a different ISN.
 uint64_t unwrap(WrappingInt32 n, WrappingInt32 isn, uint64_t checkpoint) {
-    DUMMY_CODE(n, isn, checkpoint);
-    return {};
+    uint64_t offset;
+    if (n - isn >= 0) {
+        offset = n - isn;
+    } else {
+        offset = (1ul << 32) - static_cast<uint64_t>(isn - n);
+    }
+    offset += (checkpoint >> 32) << 32;
+    if (offset < checkpoint) {
+        offset += (1ul << 32);
+    }
+    if (offset - checkpoint > (1ul << 31) && offset >= (1ul << 32)) {
+        offset -= (1ul << 32);
+    }
+    return offset;
 }
